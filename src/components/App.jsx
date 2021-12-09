c4c872dd055cae156b5f1fc00e10553b494b4cac
srglbrkchn <75715053+srglbrkchn@users.noreply.github.com>
2021-12-09 11:02:41 +0100

    Fix errors in Input area. Challange completed.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,19 +3,14 @@ import ToDoItem from "./ToDoItem";
 import InputArea from "./InputArea";
 
 function App() {
-  const [inputText, setInputText] = useState("");
+  
   const [items, setItems] = useState([]);
 
-  function handleChange(event) {
-    const newValue = event.target.value;
-    setInputText(newValue);
-  }
 
-  function addItem() {
+  function addItem(inputText) {
     setItems(prevItems => {
       return [...prevItems, inputText];
     });
-    setInputText("");
   }
 
   function deleteItem(id) {
@@ -31,7 +26,7 @@ function App() {
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
-      <InputArea />
+      <InputArea addItem={addItem} />
       <div>
         <ul>
           {items.map((todoItem, index) => (
